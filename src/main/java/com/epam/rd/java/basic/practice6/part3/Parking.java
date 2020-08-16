834f31526531501454e502ed52cbd01eaba9b388
Maksim Iandulskii <jandulm@gmail.com>
2020-08-16 16:32:04 +0300

    part3

--- src/main/java/com/epam/rd/java/basic/practice6/part3/Parking.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part3/Parking.java
@@ -1,21 +1,86 @@
 package com.epam.rd.java.basic.practice6.part3;
 
-public class Parking {
-    
-     public Parking(int capacity) {
+import java.util.Arrays;
 
+public class Parking {
+
+    private int[] places;
+
+    public Parking(int capacity) {
+        places = new int[capacity];
     }
-    
+
+    public int[] getPlaces() {
+        return places;
+    }
+
     public boolean arrive(int k) {
-        return false;
-        
+        boolean isParked;
+
+        if (k > places.length - 1 || k < 0) throw new IllegalArgumentException();
+
+        if (places[k] == 0) {
+            places[k] = 1;
+            return true;
+        }
+
+        if (k != places.length - 1) {
+            isParked = goRight(k);
+            if (!isParked) {
+                isParked = goLeft(k);
+            }
+        } else {
+            isParked = goLeft(k);
+        }
+
+        return isParked;
     }
-    
+
+    private boolean goRight(int k) {
+        boolean isParked = false;
+
+        for (int i = k + 1; i < places.length - 1; i++) {
+            if (places[i] == 0) {
+                places[i] = 1;
+                isParked = true;
+                break;
+            }
+        }
+
+        return isParked;
+    }
+
+    private boolean goLeft(int k) {
+        boolean isParked = false;
+
+        for (int i = 0; i < k; i++) {
+            if (places[i] == 0) {
+                places[i] = 1;
+                isParked = true;
+                break;
+            }
+        }
+
+        return isParked;
+    }
+
     public boolean depart(int k) {
-        return false;
+        boolean isParked = false;
+
+        if (k > places.length - 1 || k < 0) throw new IllegalArgumentException();
+
+        if (places[k] == 1) {
+            places[k] = 0;
+            isParked = true;
+        }
+
+        return isParked;
     }
-    
+
     public void print() {
-        
+
+        Arrays.stream(places).forEach(System.out::print);
+        System.out.println();
+
     }
 }
--- src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice6/part3/Part3.java
@@ -3,7 +3,24 @@ package com.epam.rd.java.basic.practice6.part3;
 public class Part3 {
     
     public static void main(String[] args) {
-        
+
+        Parking parking = new Parking(4);
+
+        System.out.println(parking.arrive(2));;
+        parking.print();
+        System.out.println(parking.arrive(3));;
+        parking.print();
+        System.out.println(parking.arrive(2));;
+        parking.print();
+        System.out.println(parking.arrive(2));;
+        parking.print();
+        System.out.println(parking.arrive(2));;
+        parking.print();
+        System.out.println(parking.depart(1));;
+        parking.print();
+        System.out.println(parking.depart(1));;
+        parking.print();
+
     }
 
 }
